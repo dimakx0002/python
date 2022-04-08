6ffd2d0c85b5b2e33113099d868d0d6ede77a451
dimakx0002 <dimakx@gmail.com>
2022-04-08 18:49:36 +0300

    test

---
+++ vacancy_mongo.py
@@ -0,0 +1,88 @@
+import re
+import requests
+import json
+from bs4 import BeautifulSoup
+from pprint import pprint
+from pymongo.errors import DuplicateKeyError
+from pymongo import MongoClient
+
+client = MongoClient('127.0.0.1', 27017)
+db = client['hh']
+professions = db.professions
+
+base_url = 'https://hh.ru/search/vacancy?area=1&fromSearchLine=true&text=Python&from=suggest_post&page='
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/98.0.4758.102 Safari/537.36'}
+
+url = f'{base_url}'+'0'
+response = requests.get(url, headers=headers)
+dom = BeautifulSoup(response.text, 'html.parser')
+
+dom.find_all('div', {'class':'vacancy-serp-item'})
+# пагинация
+nump = 0
+for item in dom.find_all('a', {'data-qa': 'pager-page'}):
+    nump = int(item.getText())
+max_page = nump
+
+vacancies_list = []
+for page in range(1):
+    url2 = f'{base_url}{page}'
+    print(url2)
+    response2 = requests.get(url2, headers=headers)
+    dom2 = BeautifulSoup(response2.text, 'html.parser')
+    vacancies2 = dom2.find_all('div', {'class': 'vacancy-serp-item'})
+    for vacancy in vacancies2:
+                vacancy_data = {}
+                vacancy_title = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text.strip()
+                vacancy_employer = vacancy.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).text.strip()
+                vacancy_link = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).get('href')
+
+                vacancy_salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+                vacancy_salary_data = {'min_salary': '', 'max_salary': '', 'currency': ''}
+                if vacancy_salary is None:
+                    vacancy_salary_data['min_salary'] = 'None'
+                    vacancy_salary_data['max_salary'] = 'None'
+                    vacancy_salary_data['currency'] = 'None'
+                else:
+                    vacancy_salary = vacancy_salary.text.replace("\u202f", '').split()
+                    if 'от' in vacancy_salary:
+                        if 'руб.' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = int(vacancy_salary[1])
+                            vacancy_salary_data['max_salary'] = 'None'
+                            vacancy_salary_data['currency'] = 'руб.'
+                        if 'USD' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = int(vacancy_salary[1])
+                            vacancy_salary_data['max_salary'] = 'None'
+                            vacancy_salary_data['currency'] = 'USD'
+                    if 'до' in vacancy_salary:
+                        if 'руб.' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = 'None'
+                            vacancy_salary_data['max_salary'] = int(vacancy_salary[1])
+                            vacancy_salary_data['currency'] = 'руб.'
+                        if 'USD' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = 'None'
+                            vacancy_salary_data['max_salary'] = int(vacancy_salary[1])
+                            vacancy_salary_data['currency'] = 'USD'
+                    if 'от' not in vacancy_salary and 'до' not in vacancy_salary:
+                        if 'руб.' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = int(vacancy_salary[0])
+                            vacancy_salary_data['max_salary'] = int(vacancy_salary[2])
+                            vacancy_salary_data['currency'] = 'руб.'
+                        if 'USD' in vacancy_salary:
+                            vacancy_salary_data['min_salary'] = int(vacancy_salary[0])
+                            vacancy_salary_data['max_salary'] = int(vacancy_salary[2])
+                            vacancy_salary_data['currency'] = 'USD'
+                vacancy_id = re.search(r'\d+', vacancy_link)
+                vacancy_data['_id'] = vacancy_id[0]
+                vacancy_data['vacancy_title'] = vacancy_title
+                vacancy_data['vacancy_employer'] = vacancy_employer
+                vacancy_data['vacancy_link'] = vacancy_link
+                vacancy_data['vacancy_salary'] = vacancy_salary_data
+                vacancies_list.append(vacancy_data)
+                try:
+                    professions.insert_one(vacancy_data)
+                except DuplicateKeyError:
+                    print(f"Document with id = {vacancy_data['_id']} already exist")
+
+pprint(len(vacancies_list))
